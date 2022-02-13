5a1e7dcf665e1936aa5bb63037641959c31c4894
imran farooq <imran_farooq7@hotmail.com>
2022-02-14 03:14:53 +0500

    HomePage Component completed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,30 @@
+import Millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
 const HomePage = () => {
-	return <h1>HomePage</h1>;
+	return (
+		<>
+			<Typography.Title level={2} className="heading">
+				Global Crypto Stats
+			</Typography.Title>
+			<Row>
+				<Col span={12}>
+					<Statistic title="Total Cryptocurrencies" value={5} />
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Exchanges" value={5} />
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Market Cap" value={5} />
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total 24h Volume" value={5} />
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Markets" value={5} />
+				</Col>
+			</Row>
+		</>
+	);
 };
 export default HomePage;
